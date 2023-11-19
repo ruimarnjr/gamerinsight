b148f7d8890b0e36478f1fa58a5cbeba3909956c
Ruimar Junior <ruimarnjr@gmail.com>
2023-11-19 15:05:13 +0000

    Added UserProfile, Game, Review, Comment, and AdminActivity models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = CloudinaryField('avatar', default='avatar/default_avatar.png')
+
+    def __str__(self):
+        return self.user.username
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100)
+    platform = models.CharField(max_length=50)
+    developer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.game.title}"
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.review.game.title}"
+
+class AdminActivity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    activity_type = models.CharField(max_length=100)
+    activity_description = models.TextField()
+    date_performed = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.activity_type}"
