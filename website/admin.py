21c96c9b85696199cf82d21c328407e41b74bb9f
Ruimar Junior <ruimarnjr@gmail.com>
2023-11-20 19:09:53 +0000

    Enhance admin panel for better management and usability

--- website/admin.py
+++ website/admin.py
@@ -2,22 +2,41 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import UserProfile, Game, Review, Comment, AdminActivity
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 1
 
-@admin.register(Game)
+class ReviewInline(admin.StackedInline):
+    model = Review
+    extra = 1
+
 class GameAdmin(SummernoteModelAdmin):
     summernote_fields = ('description',)
+    list_display = ('title', 'release_date', 'genre', 'platform', 'developer')
+    search_fields = ['title', 'developer']
+    inlines = [ReviewInline]
 
-@admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
+    list_display = ('user', 'game', 'rating', 'date_posted')
+    search_fields = ['user__username', 'game__title']
+    list_filter = ('rating', 'date_posted')
+    inlines = [CommentInline]
 
-@admin.register(Comment)
 class CommentAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
+    list_display = ('user', 'review', 'date_posted')
+    search_fields = ['user__username', 'review__game__title']
+    list_filter = ('date_posted',)
 
-@admin.register(AdminActivity)
 class AdminActivityAdmin(SummernoteModelAdmin):
     summernote_fields = ('activity_description',)
+    list_display = ('user', 'activity_type', 'date_performed')
+    search_fields = ['user__username', 'activity_type']
+    list_filter = ('date_performed',)
+
+admin.site.register(UserProfile)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(AdminActivity, AdminActivityAdmin)
