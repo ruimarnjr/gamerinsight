16d82d869b0458f137fcd767b2a2640ef30c2b5a
Ruimar Junior <ruimarnjr@gmail.com>
2023-11-21 19:25:07 +0000

    Update CSS for a more modern and clean design

--- gamerinsight/urls.py
+++ gamerinsight/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from website.views import GameListView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('website.urls'), name='website_urls')
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from .models import Game
 
-# Create your views here.
+class GameListView(generic.ListView):
+    model = Game
+    template_name = 'game_list.html'
+    context_object_name = 'games'
+
+# class GameDetailView(DetailView):
+#     model = Game
+#     template_name = 'game_detail.html'
+#     context_object_name = 'game'
+
+# class UserReviewsView(View):
+#     def get(self, request, game_id):
+#         game = get_object_or_404(Game, pk=game_id)
+#         reviews = Review.objects.filter(game=game)
+#         return render(request, 'user_reviews.html', {'game': game, 'reviews': reviews})
+
+# class UserProfileView(View):
+#     def get(self, request, user_id):
+#         user_profile = get_object_or_404(UserProfile, user_id=user_id)
+#         return render(request, 'user_profile.html', {'user_profile': user_profile})
+
+# class SubmitReviewView(View):
+#     def get(self, request, game_id):
+#         game = get_object_or_404(Game, pk=game_id)
+#         form = ReviewForm()
+#         return render(request, 'submit_review.html', {'form': form, 'game': game})
+
+#     def post(self, request, game_id):
+#         game = get_object_or_404(Game, pk=game_id)
+
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             review = form.save(commit=False)
+#             review.game = game
+#             review.user = request.user
+#             review.save()
+#             return redirect('user_reviews', game_id=game_id)
+
+#         return render(request, 'submit_review.html', {'form': form, 'game': game})
+
+# class EditReviewView(View):
+#     def get(self, request, review_id):
+#         review = get_object_or_404(Review, pk=review_id, user=request.user)
+#         form = ReviewForm(instance=review)
+#         return render(request, 'edit_review.html', {'form': form, 'review': review})
+
+#     def post(self, request, review_id):
+#         review = get_object_or_404(Review, pk=review_id, user=request.user)
+
+#         form = ReviewForm(request.POST, instance=review)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('user_reviews', game_id=review.game.pk)
+
+#         return render(request, 'edit_review.html', {'form': form, 'review': review})
+
+# class DeleteReviewView(View):
+#     def post(self, request, review_id):
+#         review = get_object_or_404(Review, pk=review_id, user=request.user)
+#         game_id = review.game.pk
+#         review.delete()
+#         return redirect('user_reviews', game_id=game_id)
+
+# class UserActivityFeedView(View):
+#     def get(self, request, user_id):
+#         activities = AdminActivity.objects.filter(user_id=user_id)
+#         return render(request, 'user_activity_feed.html', {'activities': activities})
